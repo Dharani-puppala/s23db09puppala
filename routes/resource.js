16513f5783216342bf7d2da72131a707faf17b57
Dharani-puppala <105839612+Dharani-puppala@users.noreply.github.com>
2023-11-15 14:34:19 -0600

    part 6

--- routes/resource.js
+++ routes/resource.js
@@ -21,5 +21,7 @@ router.get('/icecreams', icecream_controller.icecream_list);
 router.get('/detail', icecream_controller.icecream_view_one_Page)
 /* GET create icecream page */
 router.get('/create', icecream_controller.icecream_create_Page);
+/* GET create update page */
+router.get('/update', icecream_controller.icecream_update_Page);
 module.exports = router;
 
